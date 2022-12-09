7c758a439d1fa5829708ef25b29ea1ae48b8cefd
keramcgovern <keramcgovern@gmail.com>
2022-12-08 20:16:33 -0500

    created enum value for buliding type and updated badges to correspond to building type

---
+++ server/data/buildingCodes.js
@@ -0,0 +1,18 @@
+/**
+ * Mock ENUM values
+ * 
+ * to use:
+ *      const buildingCodes = require("./data/buildingCodes")
+ *      buildingCodes.ADMIN => returns string "ADMIN"
+ */
+
+BuildingCodes = {
+    ADMIN: "ADMIN",
+    EDUCATION: "EDUCATION",
+    HOME: "HOME",
+    GARDEN: "GARDEN",
+    STORE: "STORE",
+    PARK: "PARK"
+}
+
+module.exports = BuildingCodes
--- server/tasks/seed.js
+++ server/tasks/seed.js
@@ -6,14 +6,16 @@ const connection = require("../config/mongoConnection");
 const mongoCollections = require("../config/mongoCollections");
 const badges = mongoCollections.badges;
 
+const buildingCodes = require("../data/buildingCodes")
+
 
 async function main() {
     const db = await connection.connectToDb();
     await db.dropDatabase();
 
     try {
-        const b1 = await badgeData.createBadge(ObjectId().toString(), 50, "this is the description");
-        const b3 = await badgeData.populateBadges(ObjectId().toString(), [10, 20, 30]);
+        const b1 = await badgeData.createBadge(buildingCodes.ADMIN, 50, "this is the description");
+        const b3 = await badgeData.populateBadges(buildingCodes.EDUCATION, [10, 20, 30]);
     }
     catch (error) {
         console.log(error);
