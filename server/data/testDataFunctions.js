426a3550ba7843d6a6cddc60e7eb0f7e9bd2220f
keramcgovern <keramcgovern@gmail.com>
2022-12-10 10:57:04 -0500

    added tests for badges and updated seed file

--- server/data/testDataFunctions.js
+++ server/data/testDataFunctions.js
@@ -1,11 +1,14 @@
 const users = require('./users');
+const badgeData = require('./badges');
 
+const buildingCodes = require("./buildingCodes");
+const { ObjectId } = require("mongodb");
+const connection = require("../config/mongoConnection");
 
-async function main() {
+async function testUsers() {
 
     //-----------------------------------Check users.js-----------------------------------
     let user1, user2;
-
 
     //-----------------Checking createUser()-----------------
     try {
@@ -23,9 +26,14 @@ async function main() {
     }
 
     //-----------------Checking updateLevel()-----------------
-    
-    user2 = await users.createUser("Bella", "Cruz", "BellaTheBaddie", "TopSecret123", "Baddie@gmail.com")
-    
+
+    try {
+        user2 = await users.createUser("Bella", "Cruz", "BellaTheBaddie", "TopSecret123", "Baddie@gmail.com")
+    }
+    catch (e) {
+        console.log(e);
+    }
+
     //Bad Input Checks
     try {
         leveledUp = updateLevel();
@@ -58,7 +66,117 @@ async function main() {
     } catch (e) {
         console.log(`UpdateLevel: Test 5 Failed! ${e}`)
     }
-    
+
+}
+
+async function checkBadges() {
+    let user1, badgeSucc;
+    user1 = await users.createUser('Kera', 'McGovern', 'kmcgo', 'password1$', 'kmcgov@gmail.com')
+
+    try {
+        badgeSucc = await badgeData.createBadge(buildingCodes.ADMIN, 30, "description");
+        console.log('Badges Test 1 Success!')
+    } catch (e) {
+        console.log(`Badges Test 1 Failed! ${e}`)
+    }
+
+    try {
+        let badgeRes = await badgeData.createBadge(buildingCodes.ADMIN, null, "description");
+        console.log('Badges Test 2 Failed!')
+    } catch (e) {
+        console.log(`Badges Test 2 Success! ${e}`)
+    }
+
+    try {
+        let badgeRes = await badgeData.createBadge(30, 30, "description");
+        console.log('Badges Test 3 Failed!')
+    } catch (e) {
+        console.log(`Badges Test 3 Success! ${e}`)
+    }
+
+    try {
+        let badgeRes = await badgeData.createBadge(buildingCodes.ADMIN, 30, "");
+        console.log('Badges Test 4 Failed!')
+    } catch (e) {
+        console.log(`Badges Test 4 Success! ${e}`)
+    }
+
+    try {
+        let badgeRes = await badgeData.getBadge(badgeSucc._id);
+        console.log('Badges Test 5 Success!')
+    } catch (e) {
+        console.log(`Badges Test 5 Failed! ${e}`)
+    }
+
+    try {
+        let badgeRes = await badgeData.getBadge("");
+        console.log('Badges Test 6 Failed!')
+    } catch (e) {
+        console.log(`Badges Test 6 Success! ${e}`)
+    }
+
+    try {
+        let badgeRes = await badgeData.getBadge(ObjectID().toString());
+        console.log('Badges Test 6 Failed!')
+    } catch (e) {
+        console.log(`Badges Test 6 Success! ${e}`)
+    }
+
+    try {
+        let badgeRes = await badgeData.getAllBadges();
+        (badgeRes.length == 1) ? console.log(`Badges Test 6 Success!`) : console.log(`Badges Test 6 Failed! Should have had one value but saw ${badgeRes.length}`)
+    } catch (e) {
+        console.log(`Badges Test 6 Failed! ${e}`)
+    }
+
+    try {
+        let badgeRes = await badgeData.updateBadge(badgeSucc._id, buildingCodes.EDUCATION, 40, "dv");
+        console.log(`Badges Test 7 Success!`);
+        console.log(`old data`, badgeSucc);
+        console.log(`updated data`, badgeRes);
+    } catch (e) {
+        console.log(`Badges Test 7 Failed! ${e}`)
+    }
+
+    try {
+        let badgeRes = await badgeData.removeBadge(badgeSucc._id);
+        console.log(`Badges Test 8 Success!`);
+    } catch (e) {
+        console.log(`Badges Test 8 Failed! ${e}`)
+    }
+
+    try {
+        let badgeRes = await badgeData.getAllBadges();
+        (badgeRes.length == 0) ? console.log(`Badges Test 9 Success!`) : console.log(`Badges Test 9 Failed! Should have had one value but saw ${badgeRes.length}`)
+    } catch (e) {
+        console.log(`Badges Test 9 Failed! ${e}`)
+    }
+
+
+    try {
+        let badgeRes = await badgeData.removeBadge(ObjectId().toString());
+        console.log(`Badges Test 10 Failed!`);
+    } catch (e) {
+        console.log(`Badges Test 10 Success! ${e}`)
+    }
+
+    try {
+        let badgeRes = await badgeData.updateBadge(ObjectId().toString(), buildingCodes.ADMIN, 40, "dv");
+        console.log(`Badges Test 11 Failed!`);
+    } catch (e) {
+        console.log(`Badges Test 11 Success! ${e}`)
+    }
+
+}
+
+async function main() {
+    const db = await connection.connectToDb();
+    await db.dropDatabase();
+
+    await testUsers();
+    await checkBadges();
+
+    await connection.closeConnection();
 }
 
 main();
--- server/tasks/seed.js
+++ server/tasks/seed.js
@@ -1,10 +1,9 @@
 const data = require("../data");
 const badgeData = data.badges;
+const usersData = data.users;
 const { ObjectId } = require("mongodb");
 
 const connection = require("../config/mongoConnection");
-const mongoCollections = require("../config/mongoCollections");
-const badges = mongoCollections.badges;
 
 const buildingCodes = require("../data/buildingCodes")
 
@@ -14,8 +13,17 @@ async function main() {
     await db.dropDatabase();
 
     try {
-        const b1 = await badgeData.createBadge(buildingCodes.ADMIN, 50, "this is the description");
-        const b3 = await badgeData.populateBadges(buildingCodes.EDUCATION, [10, 20, 30]);
+        //create users
+        const user1 = await usersData.createUser('Odin', 'Crabtree', 'Odline', 'the_baby_lulu', 'urmom@gmail.com')
+        const user2 = await usersData.createUser("Bella", "Cruz", "BellaTheBaddie", "TopSecret123", "Baddie@gmail.com")
+
+        // Populate all possible badges
+        const b1 = await badgeData.populateBadges(buildingCodes.ADMIN, [20, 25, 35]);
+        const b2 = await badgeData.populateBadges(buildingCodes.EDUCATION, [10, 20, 30]);
+        const b3 = await badgeData.populateBadges(buildingCodes.STORE, [5, 15, 30]);
+        const b4 = await badgeData.populateBadges(buildingCodes.PARK, [10, 20, 30]);
+        const b5 = await badgeData.populateBadges(buildingCodes.GARDEN, [5, 10, 20]);
+        const b6 = await badgeData.populateBadges(buildingCodes.HOME, [5, 15, 30]);
     }
     catch (error) {
         console.log(error);
